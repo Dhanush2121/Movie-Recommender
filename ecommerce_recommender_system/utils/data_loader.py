493847109b2cd5a9a94b9f47f3f30ab3ad57198a
Javvadhi Dhanush <dhanushj217@gmail.com>
2025-06-19 19:21:57 +0530

    Update data_loader.py

--- ecommerce_recommender_system/utils/data_loader.py
+++ ecommerce_recommender_system/utils/data_loader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def load_data():
-    movies = pd.read_csv("data/movies.csv")
-    ratings = pd.read_csv("data/ratings.csv")
+    movies = pd.read_csv("ecommerce_recommender_system/data/movies.csv")
+    ratings = pd.read_csv("ecommerce_recommender_system/data/ratings.csv")
     return movies, ratings
+
